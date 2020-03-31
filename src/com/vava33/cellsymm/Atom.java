1235aae9ed95fa2c1233c86744fd8a8255b30067
«ovallcorba» <vallcorbao@gmail.com>
2020-03-31 14:11:00 +0200

    improve getEquivalents

--- src/com/vava33/cellsymm/Atom.java
+++ src/com/vava33/cellsymm/Atom.java
@@ -1,7 +1,7 @@
 package com.vava33.cellsymm;
 
 import java.util.ArrayList;
-import java.util.Iterator;
+import java.util.List;
 
 import org.apache.commons.math3.linear.MatrixUtils;
 import org.apache.commons.math3.linear.RealMatrix;
@@ -64,26 +64,57 @@ public class Atom {
         return false;
     }
     
-    public ArrayList<Atom> getEquivalents(SpaceGroup sg, boolean bySymmetry, boolean byLattTrans, boolean putInUnitCell){
-        ArrayList<Atom> eqAts = new ArrayList<Atom>();
-        ArrayList<Atom> allGenerated = new ArrayList<Atom>();
+    //es pot incloure simetria i/o traslacions reticulars
+    public List<Atom> getEquivalents(SpaceGroup sg, boolean bySymmetry, boolean byLattTrans, boolean putInUnitCell){
+        List<Atom> eqAts = new ArrayList<Atom>();
+        List<Atom> symmGenerated = new ArrayList<Atom>();
+        List<Atom> lattGenerated = new ArrayList<Atom>();
         this.multiplicityPosition = 0;
-        if (bySymmetry)allGenerated.addAll(sg.getSymmetryRelatedPos(this,putInUnitCell)); //ja inclou si es o no centro
-        if (byLattTrans)allGenerated.addAll(sg.getLattTransRelatedPos(this, putInUnitCell)); //afegim els de les traslacions reticulars també
-        for (Atom at1:allGenerated) {
-            boolean existing = false;
-            //mirem si ja existeix
-            for (Atom atex:eqAts) {
-                if (at1.isInTheSamePosition(atex,tolposAtoms)) {
-                    existing=true;
+        //primer aplicarem el centratge
+        if (byLattTrans) {
+            lattGenerated.addAll(sg.getLattTransRelatedPos(this, putInUnitCell)); //afegim els de les traslacions reticulars també
+        }else {
+            lattGenerated.add(this);
+        }
+        for (Atom at1:lattGenerated) {
+            //aleshores a cada atom generat pel centratge aplicarem les matrius de simetria
+            if (bySymmetry) {
+                symmGenerated.addAll(sg.getSymmetryRelatedPos(at1,putInUnitCell)); //ja inclou si es o no centro
+            }else {
+                symmGenerated.add(at1);
+            }
+            //i ara comprovem existents (volem un representant de cada únicament)
+            for (Atom at2:symmGenerated) {
+                boolean existing = false;
+                //mirem si ja existeix
+                for (Atom atex:eqAts) {
+                    if (at2.isInTheSamePosition(atex,tolposAtoms)) {
+                        existing=true;
+                    }
+                }
+                if (!existing) {
+                    eqAts.add(at2);
+                    this.multiplicityPosition++;
                 }
             }
-            if (!existing) {
-                eqAts.add(at1);
-                this.multiplicityPosition++;
+        }
+        //debug (to REMOVE)
+        log.debug("LATT TRANS + SYMM RELATED:");
+        int count = 0;
+        for (Atom a:sg.getLattTransRelatedPos(this,putInUnitCell)) {
+            for (Atom b:sg.getSymmetryRelatedPos(a, putInUnitCell)) {
+                log.debug(b.toString());
+                count++;
             }
         }
+        log.debug("total related="+count);
+        /////////--end debug block
+
         log.configf("Atom %s multiplicity= %d (SG max. %d)",this.label,this.multiplicityPosition,sg.getMaximumMultiplicity());
+        for (Atom a:eqAts) {
+            log.debug(a.toString());
+        }
+        
         return eqAts;
     }
     
@@ -111,6 +142,9 @@ public class Atom {
         return AB;
     }
     
+    public String toString() {
+        return String.format("%s %.5f %.5f %.5f ", this.label, this.xcryst,this.ycryst,this.zcryst);
+    }
     
     
     public String getLabel() {
